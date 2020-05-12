4762695d25e82e92be516b1cfa2dd37e67803811
MehmetEskar <62038737+MehmetEskar@users.noreply.github.com>
2020-05-12 18:21:41 +0200

    Update login.java

--- src/day_1/login.java
+++ src/day_1/login.java
@@ -5,6 +5,9 @@ public class login {
     public static void main(String[] args) {
 
         System.out.println("send me to gitHub");
+        
+        System.out.println("good job");
+        
     }
 
 
