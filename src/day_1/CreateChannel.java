a4b1f3ea03f29dc9a04fc1384caf92ce1eb1eac1
MehmetEskar <jvictorjose60@gmail.com>
2020-05-12 17:55:09 +0200

    add Create.Channel.java

---
+++ src/day_1/CreateChannel.java
@@ -0,0 +1,13 @@
+package day_1;
+
+public class CreateChannel {
+
+    public static void main(String[] args) {
+
+        System.out.println("user is able to create CreateChannel");
+
+        //main metodu calistirmazsak out gelmez
+        // out class .class dan
+
+    }
+}
