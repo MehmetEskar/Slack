c8d281cfe6811c154ef5ae947523cfd32ab9ea5d
MehmetEskar <jvictorjose60@gmail.com>
2020-05-15 19:01:28 +0200

    update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -9,7 +9,10 @@ public class CreateChannel {
         //main metodu calistirmazsak out gelmez
         // out class .class dannt
 
-
+        int x =5;    // added from Inttelij
+        if (x>5) {
+            System.out.println("false");
+        }
 
     }
 }
