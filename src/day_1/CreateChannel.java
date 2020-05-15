38fe1b61b7f31e5e4b3ee42a395669debbdac050
MehmetEskar <jvictorjose60@gmail.com>
2020-05-15 17:18:58 +0200

    update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -7,7 +7,12 @@ public class CreateChannel {
         System.out.println("user is able to create CreateChannel");
 
         //main metodu calistirmazsak out gelmez
-        // out class .class dan
+        // out class .class dannt
+
+        int x =5;
+        if (x>5) {
+            System.out.println("false");
+        }
 
     }
 }
